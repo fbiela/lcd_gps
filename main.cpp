de8e32fbeab69335171d91e193e27013ae260131
Frederico Sales <fbiela@gmail.com>
2017-07-27 15:14:26 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ *
+ * Frederico Sales <fbiela@gmail.com>
+ * https://www.github.com/fbiela
+ * 2017
+ * LCD SHIELD + arduino Duemilenove + GPS
+ *
+ */
+
+ #include "main.h"
+
+ // setup
+ void setup(/* arguments */) {
+   /* code */
+   setSerial();
+ }
+
+ // loop
+ void loop(/* arguments */) {
+   /* code */
+   runGPS();
+ }
